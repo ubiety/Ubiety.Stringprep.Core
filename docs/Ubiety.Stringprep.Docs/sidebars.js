d63fe08eda897b3d726290c8a060b30edfb1d7de
Dieter Lunn <coder2000@gmail.com>
2020-12-01 15:34:34 -0600

    docs: add basic pages and typescript support

--- docs/Ubiety.Stringprep.Docs/sidebars.js
+++ docs/Ubiety.Stringprep.Docs/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+  docs: {
+    "Ubiety Stringprep": ["introduction"],
+    "Getting Started": ["installation"],
   },
 };
--- docs/Ubiety.Stringprep.Docs/src/pages/index.js
+++ docs/Ubiety.Stringprep.Docs/src/pages/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
-    title: 'Versatile',
-    imageUrl: 'img/undraw_devices.svg',
+    title: "Versatile",
+    imageUrl: "img/undraw_devices.svg",
     description: (
       <>
-        Can be used with <code>.NET 5</code> or <code>.NET Standard 2.1</code> to
-        assist with processing international text in Unicode.
+        Can be used with <em>.NET 5</em> or <em>.NET Standard 2.1</em> to assist
+        with processing international text in Unicode.
       </>
     ),
   },
   {
-    title: 'Compare with Ease',
-    imageUrl: 'img/undraw_content.svg',
+    title: "Compare with Ease",
+    imageUrl: "img/undraw_content.svg",
     description: (
       <>
         Ensure that any Unicode text in your application is the same and sane.
@@ -27,20 +27,21 @@ const features = [
     ),
   },
   {
-    title: 'Profile Your Data',
-    imageUrl: 'img/undraw_process.svg',
+    title: "Profile Your Data",
+    imageUrl: "img/undraw_process.svg",
     description: (
       <>
-        Create profiles for different data types and repeat the same steps consistently.
+        Create profiles for different data types and repeat the same steps
+        consistently.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -54,22 +55,24 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="RFC 3454 implementation for .NET">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      description="RFC 3454 implementation for .NET"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl("docs/")}
+            >
               Get Started
             </Link>
           </div>
