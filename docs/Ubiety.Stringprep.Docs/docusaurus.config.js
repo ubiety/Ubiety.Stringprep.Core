ac0756ea28b4ccc86f524ffa39927233a8c04cb1
Dieter Lunn <coder2000@gmail.com>
2020-12-02 07:20:20 -0600

    docs: update docusaurus config

--- docs/Ubiety.Stringprep.Docs/docusaurus.config.js
+++ docs/Ubiety.Stringprep.Docs/docusaurus.config.js
@@ -1,78 +1,70 @@
 module.exports = {
-  title: 'Ubiety Stringprep',
-  tagline: 'RFC 3454 for .NET',
-  url: 'https://stringprep.ubiety.ca',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'ubiety', // Usually your GitHub org/user name.
-  projectName: 'ubiety.stringprep.core', // Usually your repo name.
+  title: "Ubiety Stringprep",
+  tagline: "RFC 3454 for .NET",
+  url: "https://stringprep.ubiety.dev",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "ubiety", // Usually your GitHub org/user name.
+  projectName: "ubiety.stringprep.core", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'Ubiety Stringprep',
+      title: "Ubiety Stringprep",
       logo: {
-        alt: 'Stringprep Logo',
-        src: 'img/rope.svg',
+        alt: "Stringprep Logo",
+        src: "img/rope.svg",
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        { to: "blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/ubiety/ubiety.stringprep.core',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/ubiety/ubiety.stringprep.core",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
+              label: "Introduction",
+              to: "docs/",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/ubiety",
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: "Discord",
+              href: "https://discord.gg/VNtMymHdnG",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Blog",
+              to: "blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/ubiety/ubiety.stringprep.core',
+              label: "GitHub",
+              href: "https://github.com/ubiety/ubiety.stringprep.core",
             },
           ],
         },
@@ -82,22 +74,22 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/ubiety/Ubiety.Stringprep.Core/edit/develop/docs/Ubiety.Stringprep.Docs/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/ubiety/Ubiety.Stringprep.Core/edit/develop/docs/Ubiety.Stringprep.Docs/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
